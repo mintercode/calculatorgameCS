fa4c8f764ac51ebbe4ee5d53b2eff259fc388c22
Tommy <thomasminter.m1@gmail.com>
2023-02-25 18:32:29 -0800

    Practice

---
+++ moshtest/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Another Number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadLine();
+        }
+
+    } 
+}
